bfb487034acdb4708a6f2341f6ddbf896c9555d6
Hunain <hunaindev1@gmail.com>
2023-09-18 16:10:19 +0500

    debug

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use('/assets', express.static('assets'));
 
 app.use(helmet())
 app.use(limiter);
-app.use(cors());
+app.use(cors({origin: 'http://localhost:4200'}));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET , PUT , POST , DELETE");
