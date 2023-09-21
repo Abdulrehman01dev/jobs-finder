5843dbc88f590b17da4cd87d2edc32decc766131
Hunain <hunaindev1@gmail.com>
2023-09-21 16:45:09 +0500

    debug

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.set('trust proxy', false)
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
 	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    validate: {xForwardedForHeader: false}
 })
 
 
